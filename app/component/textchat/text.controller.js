1c7ac3878a9ea76d7b3ccec04ae51274616399d1
andela-ofamakinde <olatoyosi.famakinde@andela.co>
2015-04-14 17:14:34 +0000

    getUserMedia() activated

--- app/component/textchat/text.controller.js
+++
@@ -1,3 +0,0 @@
-myApp.controller("textCtrl", ["$scope", "textServices", function($scope, textServices){
-
-}]);
--- app/component/textchat/text.service.js
+++
@@ -1,7 +0,0 @@
-var textServices = angular.module("textServices", []);
-DC1.factory = ("dataChannel", ["",function(){
-
-}]);
-textServices.factory = ("textfactory", ["" , function(){
-
-}])
