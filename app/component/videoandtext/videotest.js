fe5b83d5d74a9088cd7e502736e2c1a90c10460e
andela-ofamakinde <olatoyosi.famakinde@andela.co>
2015-04-20 13:33:42 +0000

    Unit test

--- app/component/videoandtext/videotest.js
+++ app/component/videoandtext/videotest.js
@@ -2,25 +2,13 @@ describe("Unit: Testing Controllers", function() {
 
   beforeEach(module('myApp'));
 
-  // it('should have a VtCtrl controller', function() {
-  //   expect(myApp.VtCtrl).not.to.equal(null);
-  // });
+   it('should have a VtCtrl controller', function() {
+     expect(myApp.VtCtrl).not.to.equal(null);
+  });
   var $controller;
 
   beforeEach(inject(function(_$controller_) {
     $controller = _$controller_;
   }));
-
-  describe('$scope.me', function() {
-    it('initialize the text messaging', function() {
-     var $scope = {};
-     var controller = $controller('VtCtrl', {$scope: $scope});
-     $scope.username = 'ayoola';
-     $scope.me();
-     expect($scope.me).toEqual($scope.username);
-    });
-  });
-
-
 
 });
